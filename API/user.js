ef1848ab77796efdd2d107f4c2851ff1d72c9332
Devisowmya-547 <115868465+Devisowmya-547@users.noreply.github.com>
2024-05-11 14:25:35 +0530

    Add files via upload

---
+++ API/user.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const userApp = express.Router();
+const bcryptjs = require('bcryptjs');
+const { useRevalidator } = require('react-router-dom');
+
+let refEmail;
+let userCollection;
+
+userApp.use((req, res, next) => {
+  userCollection = req.app.get('userCollection');
+  next();
+})
+
+userApp.post('/register', async (req, res) => {
+  const newUser = req.body;
+  const email = await userCollection.findOne({email : newUser.email});
+  if(email !== null){
+    return res.send({message : email})
+  }
+  newUser.password = await bcryptjs.hash(newUser.password, 7);
+  await userCollection.insertOne(newUser);
+  res.send({message : 'Account created successfully'})
+})
+
+userApp.post('/login', async (req, res) => {
+  const user = req.body;
+  const validUser = await userCollection.findOne({email : user.email});
+  if(validUser === null) { return res.send({message : 'user not found'});}
+  const passCheck = await bcryptjs.compare(user.password, validUser.password);
+  if(passCheck === false) { return res.send({message : 'Invalid password'});}
+  refEmail = user.email;
+  res.send({message : 'exist'})
+})
+
+userApp.put('/exercise', async (req, res) => {
+  const exercise = req.body;
+  await userCollection.findOneAndUpdate(
+    {email : refEmail},
+    {$addToSet : {exercises : exercise.exer}},
+    {ReturnDocument : 'after'}
+  )
+})
+
+module.exports = userApp;
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const path = require('path');
+const userApp = require('./API/user');
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+const uri = 'mongodb://localhost:27017';
+MongoClient.connect(uri)
+.then((client) => {
+  const database = client.db('fitnessApp');
+
+  const userCollection = database.collection('userCollection');
+
+  app.set('userCollection', userCollection);
+  console.log('DB connection established');
+})
+.catch((error) => {console.log(error)})
+
+app.listen(8000, () => {
+  console.log('listening on port 8000');
+})
+
+app.use('/user', userApp)
+
+app.use((err, req, res, next) => {
+  res.send(`error generated ${err}`)
+})
